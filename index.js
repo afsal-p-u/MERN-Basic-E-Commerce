f37772f28b7a239d817094fb52f7ee543c409fbc
Afsal P U <afsalpu16@gmail.com>
2023-02-15 23:47:55 +0530

    routers and stripe payment method added

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 const authRouter = require('./routes/auth')
+const userRouter = require('./routes/user')
+const productRouter = require('./routes/product')
+const cartRouter = require('./routes/cart')
+const orderRouter = require('./routes/order')
 
 // env cofiguration
 require('dotenv').config()
@@ -25,6 +29,10 @@ app.use(express.json())
 
 // routers
 app.use('/api/user', authRouter)
+app.use('/api/user', userRouter)
+app.use('/api/products', productRouter)
+app.use('/api/carts', cartRouter)
+app.use('/api/orders', orderRouter)
 
 
 const port = process.env.PORT || 8080
